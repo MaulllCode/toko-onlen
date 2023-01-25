378148afda6965fb699dbfe2867580a7abbe1385
Maulana <maulaadjis52@gmail.com>
2023-01-25 15:50:26 +0700

    first commit

---
+++ application/controllers/dashboard.php
@@ -0,0 +1,14 @@
+<?php
+class Dashboard extends CI_controller {
+
+	public function index() 
+	{
+		$model_barang = 'model_barang';
+		$data['barang'] = $this->$model_barang->tampil_data()->result();
+		$this->load->view('template/header');
+		$this->load->view('template/sidebar');
+		$this->load->view('dashboard', $data);
+		$this->load->view('template/footer');
+	}
+}
+?>
